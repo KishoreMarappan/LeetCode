4d969a0eac0508c0354814511dcfafa90f478e91
KishoreMarappan <137819447+KishoreMarappan@users.noreply.github.com>
2024-07-05 10:11:08 +0530

    Time: 0 ms (100.00%) | Memory: 40.8 MB (11.70%) - LeetSync

---
+++ 1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        int c=1;
+        if(k==1) return 1;
+        for(int i=2;i<=n;i++){
+            if(n%i==0){
+                c++;
+            }
+            if(c==k) return i;
+        }
+        return -1;
+    }
+}
